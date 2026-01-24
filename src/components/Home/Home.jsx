44abd7e7e79619589525cf51a1ce00178db9a8e3
mohammed sirjudeen <mohammedsira18@gamil.com>
2026-01-24 20:33:29 +0530

    sira portfolio

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -45,7 +45,7 @@ const About = () => {
                     </p>
                     {/* Resume Button */}
                     <a
-                        href="https://drive.google.com/file/d/1AzN8g-r9rr1e6puZnGFB4azorG-7DX4P/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1PpAnL2ymsrGHH25Bygw4IPgruo5PQueU/view?usp=sharing"
                         target="_blank"
                         rel="noopener noreferrer"
                         download
